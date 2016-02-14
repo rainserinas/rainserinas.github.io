24a62017d7a6b4a1cd5720879af2f08af385e448
rainserinas <raineirserinas@gmail.com>
2016-02-13 23:57:03 -0800

    apply basic task on grunt file

--- sample_contact/Gruntfile.js
+++ sample_contact/Gruntfile.js
@@ -11,14 +11,33 @@ module.exports = function(grunt) {
                 ],
                 dest: 'js/build/production.js',
             }
+        },
+
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            }
         }
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat','uglify','watch']);
 
 };
