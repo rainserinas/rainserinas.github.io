3ef5d7b3c43a131f933d042855af0091fc32b3a2
rainserinas <raineirserinas@gmail.com>
2016-02-13 23:42:10 -0800

    sample contact form with material design

---
+++ sample_contact/Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'js/*.js',
+                ],
+                dest: 'js/build/production.js',
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat']);
+
+};
