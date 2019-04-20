ecabb211a32bcb1982ef12b65bc45ef3e133891a
rainserinas <raineirserinas@gmail.com>
2019-04-20 20:59:48 +0800

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,13 +58,4 @@ jQuery(document).ready(function($) {
         
         }
     );
-    
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
-    
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
-
-
 });
